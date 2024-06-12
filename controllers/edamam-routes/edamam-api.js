260f833e565969e0a355864737b6d6e26290511e
anullator <danyelle.joy@gmail.com>
2024-06-11 21:03:16 -0500

    created recipe request to edamam api

---
+++ controllers/edamam-routes/edamam-api.js
@@ -0,0 +1,15 @@
+const baseURL = 'https://api.edamam.com/api/recipes/v2';
+
+async function getRecipes(params) {
+    try {
+        const url = `${baseURL}?type=public&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_APP_KEY}&${params}`;
+        const response = await fetch(url);
+        const result = await response.json();
+        console.log(result);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+module.exports = getRecipes;

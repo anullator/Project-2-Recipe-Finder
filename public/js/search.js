ad0b7cfe99fb19014093fce831ed9d28e42e22ca
anullator <danyelle.joy@gmail.com>
2024-06-16 15:33:47 -0500

    fixed search event listener and created function to get the inputs and format for edamam api call

--- public/js/search.js
+++ public/js/search.js
@@ -1,11 +1,45 @@
-const handleSearch = (event) => {
-    console.log(`enter handle search`);
+const getRecipes = require('./controllers/edamam-routes/edamam-api');
+
+async function handleSearch (event) {
     event.preventDefault();
-    const query = document.getElementById("keyword");
-    console.log(query);
-    const diet = document.getElementById("diet");
-    console.log(diet);
+    let param = '';
+    let selectedId;
+
+    // get keyword input
+    const rawQuery = document.getElementById("keyword").value.trim();
+
+    //separate multi-word input and recombine
+    let parts = rawQuery.split(' ');
+    let query = parts.join('+');
+
+    // get selected radio btn
+    const selectedDiet = document.querySelector('input[name="diet"]:checked');
+
+    // get val of selected radio btn
+    if (selectedDiet) {
+        selectedId = selectedDiet.id;
+    } 
+
+    // check if keyword input exists
+    if (query && !selectedId) {
+        param+= `q=${query}`;
+    
+    // check if keyword and diet both exist
+    } else if (query && selectedId) {
+        param+= `q=${query}&diet=${selectedId}`;
+
+    // check if only diet exists
+    } else if (!query && selectedId) {
+        param+= `diet=${selectedId}`;
+
+    // error handling if search attempted w/neither keyword or diet
+    } else {
+        alert('Must enter keyword or select a diet.');
+        return;
+    }
+    
+    // getRecipes(param);
 }
 
+// attach click listener to search button
 document.querySelector('#search').addEventListener('click', handleSearch);
-console.log(`added event listener`);
--- server.js
+++ server.js
@@ -46,3 +46,4 @@ sequelize.sync({ force: false })
     .then(() => {
         app.listen(PORT, () => console.log(`Now listening on port http://localhost:${PORT}`));
     }).catch(err => console.log('Error:', err));
+    
